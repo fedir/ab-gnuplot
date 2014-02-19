66032b7332b2a95a1d7aced457a1a7b173b758eb
Fedir RYKHTIK <fedir@stratis.fr>
2014-02-19 18:43:11 +0100

    Logs activated

--- test100k.sh
+++ test100k.sh
@@ -1,6 +1,6 @@
 for concurency in 1 10 20 50 100
 do
-	ab -n 100 -c $concurency -g data/test100k-$concurency.dat $1
+	ab -n 100 -c $concurency -g data/test100k-$concurency.dat $1 > log/test100k-$concurency.log
 done
 gnuplot test100kLinear.p
 gnuplot test100kTimeline.p
